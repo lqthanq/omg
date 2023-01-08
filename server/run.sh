6749933d0fcdf69eef900cc77c1260f7086c88b0
Thang Le <thangbiet17@gmail.com>
2023-01-08 07:37:17 +0700

    Update run.sh

--- server/run.sh
+++ server/run.sh
@@ -1,3 +1,6 @@
-export POSTGRES_CONNECTION_URL="host=localhost port=5432 user=postgres dbname=omg-simpler password=Thang@1102 sslmode=disable"
+# export POSTGRES_CONNECTION_URL="host=localhost port=5432 user=postgres dbname=omg-simpler password=Thang@1102 sslmode=disable"
+export POSTGRES_CONNECTION_URL="host=dpg-cesrh6da499cn4gmrsg0-a.oregon-postgres.render.com port=5432 user=omg_simpler_user dbname=omg_simpler password=PvBWlG0JADAhjQoZoffvmlZzKF7vVifk sslmode=require"
+
+# postgres://omg_simpler_user:PvBWlG0JADAhjQoZoffvmlZzKF7vVifk@dpg-cesrh6da499cn4gmrsg0-a.oregon-postgres.render.com/omg_simpler
 
 go run ./server/server.go
